dd35d9c23b4ec72adb105b9f97a52979526a53e3
Alex Bannon <alexbannon@gmail.com>
2015-07-12 16:46:39 -0400

    added playing cards, added fisher-yates shuffle algorithm, added shoe of 6 decks out of shuffled 8

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+var clickCounter = 0;
 $("#playerBox").on("click", function(){
-  var whichCard = getRandomInt(1, 53);
-  $("#playerBox").html(whichCard);
-  console.log(whichCard);
+  $("#playerBox").empty();
+  var whichNumber = shoeOfSixDecks[clickCounter] + ".png";
+  $("#playerBox").append('<img id="playerOneCard"src="playing_cards/'+whichNumber+'" />')
+  clickCounter++;
 })
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+function shuffleCards(array) {
+  for(var i = array.length - 1; i > 0; i--) {
+    var pickRandomIndex = Math.floor(Math.random() * (i + 1));
+    var holdValueToSwitchWithRandomIndexLater = array[i];
+    array[i] = array[pickRandomIndex]; //switch out array[i] with new value, randomly chosen
+    array[pickRandomIndex] = holdValueToSwitchWithRandomIndexLater; // Put switched out value back into array at this iterations random index choice
+  }
+  return array;
+}
+var arrayOfPlayingCards = [];
+for(var i = 1; i < 417; i++) {
+  arrayOfPlayingCards.push(i);
+}
+shuffleCards(arrayOfPlayingCards);
+arrayOfPlayingCards = arrayOfPlayingCards.map(divideByEightAndRoundUp);
+var shoeOfSixDecks = arrayOfPlayingCards.slice(0,312);
+console.log(shoeOfSixDecks);
+function divideByEightAndRoundUp(input) {
+  return Math.ceil(input / 8);
 }
